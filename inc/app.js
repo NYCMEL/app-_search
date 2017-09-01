a19614662f3850fadccb2d676a3078a7a5948f36
Mel <mel.heravi@gmail.com>
2017-09-01 14:22:53 -0400

    committed

--- inc/app.js
+++ inc/app.js
@@ -1,7 +1,7 @@
 var app = {};
 
 /////////////////////////////////////////////////////////////////////////////////
-//// Time-stamp: <2017-09-01 14:21:25 (melify)>
+//// Time-stamp: <2017-09-01 14:21:51 (melify)>
 /////////////////////////////////////////////////////////////////////////////////
 app.init = function() {
     console.group("app.init");
@@ -12,7 +12,7 @@ app.init = function() {
 
     wc.subscribe("wc-header", function(e) {
 	console.info("'wc-header' subscription was triggered:", JSON.stringify(e.detail));
-
+	
 	$(".apage").hide(0, function() {
 	    $("#page-" + e.detail.id).show(0);
 	});
