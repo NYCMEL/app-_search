2bbd9ad9053f62ff89b6e33e76c41ec4701049e7
Mel <mel.heravi@gmail.com>
2017-11-05 09:37:43 -0500

    committed

--- app.vfs/lib/app/test.tcl
+++ app.vfs/lib/app/test.tcl
@@ -72,41 +72,15 @@ m::proc -public test::panel {
     Trace
     variable _id [id]
 
-    put {
-	<wc-panel id="my-panel-1" collapsible="true" height="250px" class="uxcoe">
-	<wc-panel-header>My Panel Header</wc-panel-header>
-	<wc-panel-body>
-	<h5>Panel content goes here</h5>
-	Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-	tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
-	At vero eos et accusam et justo duo dolores et ea rebum.
-	Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-	Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-	sed diam nonumy eirmod tempor 
-	</wc-panel-body>
-	<wc-panel-footer>My Panel Footer</wc-panel-footer>
-	</wc-panel>
-    }
-}
-
-######################################################
-##### 
-######################################################
-m::proc -public test::panel:s {
-} {
-    Documentation goes here...
-} {    
-    Trace
-    variable _id [id]
-
     br
     division class="container" {
 	division class="row" {
-	    division class="col-md-12" {
+	    division class="col-md-6" {
 		set hstr {
-		    <wc-panel id="$v(id)" collapsible="$v(collapsible)" height="$v(height)">
+		    <wc-panel id="$v(id)" collapsible="$v(collapsible)" height="$v(height)" class="uxcoe">
 		    <wc-panel-header>$v(header)</wc-panel-header>
 		    <wc-panel-body>
+		    <h5>$v(pheader)</h5>
 		    $v(body)
 		    </wc-panel-body>
 		    <wc-panel-footer>$v(footer)</wc-panel-footer>
@@ -116,15 +90,12 @@ m::proc -public test::panel:s {
 		array set v [subst {
 		    id		"panel-1"
 		    collapsible	"true"
-		    height		"400px"
-		    header		"Mel Was Here"
-		    body		"[lorem 30]"
-		    footer		"Mel was here too..."
+		    height	"200px"
+		    header	"Panel Title Goes here..."
+		    pheader	"body header"
+		    body	"[lorem 50]"
+		    footer	"Mel was here too..."
 		}]
-
-		parray v
-
-		put "<pre>[quote_html [subst $hstr]]</pre>"
 
 		put [subst $hstr]
 	    }
